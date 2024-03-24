d4ba2d9f293d3e0cd03f3f43b32ce217fa88773e
GeorgeKahugu <gee.kahugu@gmail.com>
2024-03-24 08:17:05 +0300

    initial commit

--- Week1:code-challenge/StudentCodeGenerator.js
+++ Week1:code-challenge/StudentCodeGenerator.js
@@ -1,20 +1,24 @@
+// // Inputing Student Marks. Then having an output of the correct grade.
+// // Input Function
 function inputStudentmark(studentmark) {
     
     let grade;
-
+// if statement
     if (studentmark >= 79 && studentmark <= 100) {
-        grade = "A";
-    } else if (studentmark >= 60 && studentmark < 79) {
-        grade = "B";
+        grade = "A"
+// else if statements
+    } else if (studentmark >= 60 && studentmark <=79) {
+        grade = "B"
     } else if (studentmark >= 50 && studentmark <= 59) {
-        grade = "C";
+        grade = "C"
     } else if (studentmark >= 40 && studentmark < 49) {
-        grade = "D";
+        grade = "D"
     } else if (studentmark < 0 || studentmark > 100) {
-          grade = "E";
+          grade = "E"
     }
 
     console.log(`The student's mark is ${studentmark} and the grade is: ${grade}`);
 }
 
+console.log(inputStudentmark(60))
 
